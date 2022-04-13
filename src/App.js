8e247a8f855868e44eab61751f7eccadd578d3c9
Ferran Casals <ferrancasalsfajula@gmail.com>
2022-04-14 00:38:13 +0200

    Metamask

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Grid from "./components/Grid"
+import {Button} from 'react-bootstrap'
+import useMetamask from './hooks/metamask';
+
 
 function App() {
+
+  const { connect, disconnect, isActive, account, shouldDisable } = useMetamask()
+
+
   return (
     <div className="App">
+      <header className="App-header">
+        <Button variant="success" onClick={connect} disabled={shouldDisable}>
+          Connect Wallet
+        </Button>
+        <div className="mt-2 mb-2"> Connected Account: { isActive ? account : '' }</div>
+        <Button variant="danger" onClick={disconnect}>
+          Disconnect Wallet
+        </Button>
+      </header> 
       <Grid />
     </div>
   );
