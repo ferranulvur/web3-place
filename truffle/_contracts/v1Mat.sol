f3b81986c8fe47d812d5b3d9faca371a851484c8
Ferran Casals <ferrancasalsfajula@gmail.com>
2022-05-04 15:07:15 +0800

    New SC

--- truffle/_contracts/v1Mat.sol
+++ truffle/_contracts/v1Mat.sol
@@ -2,24 +2,24 @@
 pragma solidity ^0.8.0;
  
 contract Matrix {
-    uint8 private constant SIZE = 100; 
-    uint8[SIZE][SIZE] private matrix;
+    uint8 private constant SIZE = 20; 
+    string[SIZE][SIZE] private matrix;
     // mapping(uint256 => uint256) public colours;
     // mapping(bytes32 => bytes32) public colours;
  
     constructor() {
         for(uint8 i = 0; i < SIZE; i++){
             for(uint8 j = 0; j < SIZE; j++){
-                matrix[i][j] = 0;
+                matrix[i][j] = "#ffffff";
             }
         }
     }
  
-    function getMatrix() external view returns(uint8[SIZE][SIZE] memory){
+    function getMatrix() external view returns(string[SIZE][SIZE] memory){
         return matrix;
     }
  
-    function setMatrixNumber(uint8 x, uint8 y, uint8 newValue) external {
+    function setMatrixNumber(uint8 x, uint8 y, string memory newValue) external {
         require(x <= SIZE && y >= 0, "MATRIX: Out of bounds");
         require(x <= SIZE && x >= 0, "MATRIX: Out of bounds");
         matrix[x][y] = newValue;
